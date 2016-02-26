5de0bfc43eae3d10c24c5db0c922dcd8ddd74bdc
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-02-26 11:29:42 +0200

    say goodbye to Angular 1, Hello NG2

--- app/scripts/bootstrap.ts
+++ app/scripts/bootstrap.ts
@@ -1,16 +1,5 @@
-declare var angular: any;
 
-import {UpgradeAdapter} from 'angular2/upgrade';
-import {TodoList} from "./components/todo-list";
-import {TodoInput} from "./components/todo-input";
+import { bootstrap } from 'angular2/platform/browser';
 import {TodoApp} from "./components/todo-app";
 
-let adapter = new UpgradeAdapter();
-
-angular.module('todoListWorkshopApp').directive('todoList', adapter.downgradeNg2Component(TodoList));
-
-angular.module('todoListWorkshopApp').directive('todoInput', adapter.downgradeNg2Component(TodoInput));
-
-angular.module('todoListWorkshopApp').directive('todoApp', adapter.downgradeNg2Component(TodoApp));
-
-adapter.bootstrap(document.body, ['todoListWorkshopApp']);
+bootstrap(TodoApp);
